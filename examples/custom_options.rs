d1d0ccf396d7fa522c7d7ecd917f8de70d5a0fd5
LeopoldArkham <leopold.arkham@gmail.com>
2016-11-19 23:08:52 +0100

    Added example for a custom option set

---
+++ examples/custom_options.rs
@@ -0,0 +1,21 @@
+extern crate humansize;
+use humansize::{FileSize, file_size_opts as opts};
+
+fn main() {
+	// Declare a fully custom option struct
+	let custom_options = opts::FileSizeOpts {
+		divider: opts::Standard::Binary,
+		units: opts::Standard::Decimal,
+		decimal_places: 3,
+		decimal_zeroes: 1,
+		long_suffix: true
+	};
+	// Then use it
+	println!("{}", 3024.file_size(custom_options).unwrap());
+
+
+	// Or use only some custom parameters and adopt the rest from an existing config
+	let semi_custom_options = opts::FileSizeOpts {decimal_zeroes: 3, ..opts::BINARY};
+
+	println!("{}", 2024.file_size(semi_custom_options).unwrap());
+}

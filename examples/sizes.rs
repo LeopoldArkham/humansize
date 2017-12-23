e89c7dd86329727d5fd4686bbbd385e006a808fb
Leopold Arkham <leopold.arkham@gmail.com>
2017-12-23 19:44:22 +0100

    Allow negative input, rustfmt

--- examples/sizes.rs
+++ examples/sizes.rs
@@ -3,11 +3,19 @@ extern crate humansize;
 use humansize::{FileSize, file_size_opts};
 
 fn main() {
-	// Call the file_size method on any non-negative integer with the option set you require
+    // Call the file_size method on any non-negative integer with the option set you require
 
-	println!("{}", 5456.file_size(file_size_opts::BINARY).unwrap());
-	println!("{}", 1024.file_size(file_size_opts::BINARY).unwrap());
-	println!("{}", 1000.file_size(file_size_opts::DECIMAL).unwrap());
-	println!("{}", 1023_654_123_654u64.file_size(file_size_opts::DECIMAL).unwrap());
-	println!("{}", 123456789.file_size(file_size_opts::CONVENTIONAL).unwrap());
+    println!("{}", 5456.file_size(file_size_opts::BINARY).unwrap());
+    println!("{}", 1024.file_size(file_size_opts::BINARY).unwrap());
+    println!("{}", 1000.file_size(file_size_opts::DECIMAL).unwrap());
+    println!(
+        "{}",
+        1023_654_123_654u64
+            .file_size(file_size_opts::DECIMAL)
+            .unwrap()
+    );
+    println!(
+        "{}",
+        123456789.file_size(file_size_opts::CONVENTIONAL).unwrap()
+    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -32,34 +32,38 @@
 //! and pass that to the method. See the `custom_options.rs` file in the example folder.
 
 static SCALE_DECIMAL: [&'static str; 9] = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-static SCALE_DECIMAL_LONG: [&'static str; 9] = ["Bytes",
-                                                "Kilobytes",
-                                                "Megabytes",
-                                                "Gigabytes",
-                                                "Terabytes",
-                                                "Petabytes",
-                                                "Exabytes",
-                                                "Zettabytes",
-                                                "Yottabytes"];
+static SCALE_DECIMAL_LONG: [&'static str; 9] = [
+    "Bytes",
+    "Kilobytes",
+    "Megabytes",
+    "Gigabytes",
+    "Terabytes",
+    "Petabytes",
+    "Exabytes",
+    "Zettabytes",
+    "Yottabytes",
+];
 
-static SCALE_BINARY: [&'static str; 9] = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB",
-                                          "YiB"];
-static SCALE_BINARY_LONG: [&'static str; 9] = ["Bytes",
-                                               "Kibibytes",
-                                               "Mebibytes",
-                                               "Gibibytes",
-                                               "Tebibytes",
-                                               "Pebibytes",
-                                               "Exbibytes",
-                                               "Zebibytes",
-                                               "Yobibytes"];
+static SCALE_BINARY: [&'static str; 9] =
+    ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
+static SCALE_BINARY_LONG: [&'static str; 9] = [
+    "Bytes",
+    "Kibibytes",
+    "Mebibytes",
+    "Gibibytes",
+    "Tebibytes",
+    "Pebibytes",
+    "Exbibytes",
+    "Zebibytes",
+    "Yobibytes",
+];
 
 
 pub mod file_size_opts {
     //! Describes the struct that holds the options needed by the `file_size` method.
     //! The three most common formats are provided as constants to be used easily
 
-    #[derive(Debug, PartialEq)]
+    #[derive(Debug, PartialEq, Copy, Clone)]
     /// Holds the standard to use when displying the size.
     pub enum Kilo {
         /// The decimal scale and units
@@ -83,12 +87,12 @@ pub mod file_size_opts {
         No,
     }
 
-    /// Holds the options to be passed to the `file_size` method.
+    /// Holds the options for the `file_size` method.
     #[derive(Debug)]
     pub struct FileSizeOpts {
-        /// The scale to compare the size against.
+        /// The scale (binary/decimal) to divide against.
         pub divider: Kilo,
-        /// The unit set to use.
+        /// The unit set to display.
         pub units: Kilo,
         /// The amount of decimal places to display if the decimal part is non-zero.
         pub decimal_places: usize,
@@ -100,8 +104,16 @@ pub mod file_size_opts {
         pub long_units: bool,
         /// Whether to place a space between value and units.
         pub space: bool,
-        /// Optional suffix at the end
+        /// An optional suffix which will be appended after the unit.
         pub suffix: &'static str,
+        /// Whether to allow negative numbers as input. If `False`, negative values will return an error.
+        pub allow_negative: bool,
+    }
+
+    impl AsRef<FileSizeOpts> for FileSizeOpts {
+        fn as_ref(&self) -> &FileSizeOpts {
+            self
+        }
     }
 
     /// Options to display sizes in the binary format.
@@ -114,6 +126,7 @@ pub mod file_size_opts {
         long_units: false,
         space: true,
         suffix: "",
+        allow_negative: false,
     };
 
     /// Options to display sizes in the decimal format.
@@ -126,10 +139,11 @@ pub mod file_size_opts {
         long_units: false,
         space: true,
         suffix: "",
+        allow_negative: false,
     };
 
-    /// Options to display sizes in the conventional format.
-    /// This uses multiples of 1024 to calculate the scale, but displays decimal units (`KB`, not `KiB`).
+    /// Options to display sizes in the "conventional" format.
+    /// This 1024 as the value of the `Kilo`, but displays decimal-style units (`KB`, not `KiB`).
     pub const CONVENTIONAL: FileSizeOpts = FileSizeOpts {
         divider: Kilo::Binary,
         units: Kilo::Decimal,
@@ -139,6 +153,7 @@ pub mod file_size_opts {
         long_units: false,
         space: true,
         suffix: "",
+        allow_negative: false,
     };
 }
 /// The trait for the `file_size`method
@@ -148,7 +163,8 @@ pub trait FileSize {
     /// to your needs
     ///
     /// # Errors
-    /// Will fail if called on a negative number
+    /// Will fail by default if called on a negative number. Override this behavior by setting
+    /// `allow_negative` to `True` in a custom options struct.
     ///
     /// # Examples
     /// ```rust
@@ -158,7 +174,7 @@ pub trait FileSize {
     /// println!("Size is {}", size.file_size(options::DECIMAL).unwrap());
     /// ```
     ///
-    fn file_size(&self, opts: FileSizeOpts) -> Result<String, String>;
+    fn file_size<T: AsRef<FileSizeOpts>>(&self, opts: T) -> Result<String, String>;
 }
 
 use self::file_size_opts::*;
@@ -166,7 +182,8 @@ use self::file_size_opts::*;
 macro_rules! impl_file_size_u {
     (for $($t:ty)*) => ($(
         impl FileSize for $t {
-        	fn file_size(&self, opts: FileSizeOpts) -> Result<String, String> {
+        	fn file_size<T: AsRef<FileSizeOpts>>(&self, _opts: T) -> Result<String, String> {
+                let opts = _opts.as_ref();
         		let divider = match opts.divider {
         			Kilo::Decimal => 1000.0,
         			Kilo::Binary => 1024.0
@@ -220,9 +237,19 @@ macro_rules! impl_file_size_u {
 macro_rules! impl_file_size_i {
     (for $($t:ty)*) => ($(
         impl FileSize for $t {
-        	fn file_size(&self, opts: FileSizeOpts) -> Result<String, String> {
-        		if *self < 0 {return Err("Tried calling file_size on a negative value".to_owned());}
-        		(*self as u64).file_size(opts)
+        	fn file_size<T: AsRef<FileSizeOpts>>(&self, _opts: T) -> Result<String, String> {
+                let opts = _opts.as_ref();
+        		if *self < 0 && !opts.allow_negative { 
+                    return Err("Tried calling file_size on a negative value".to_owned());
+                } else {
+                    let sign = if *self < 0 {
+                        "-"
+                    } else {
+                        ""
+                    };
+
+        		    Ok(format!("{}{}", sign, (self.abs() as u64).file_size(opts)?))
+                }
 
     		}
 	    }
@@ -244,12 +271,16 @@ fn test_sizes() {
     assert_eq!(1024.file_size(BINARY).unwrap(), "1 KiB");
     assert_eq!(1024.file_size(CONVENTIONAL).unwrap(), "1 KB");
 
-    let semi_custom_options =
-        file_size_opts::FileSizeOpts { space: false, ..file_size_opts::DECIMAL };
+    let semi_custom_options = file_size_opts::FileSizeOpts {
+        space: false,
+        ..file_size_opts::DECIMAL
+    };
     assert_eq!(1000.file_size(semi_custom_options).unwrap(), "1KB");
 
-    let semi_custom_options2 =
-        file_size_opts::FileSizeOpts { suffix: "/s", ..file_size_opts::BINARY };
+    let semi_custom_options2 = file_size_opts::FileSizeOpts {
+        suffix: "/s",
+        ..file_size_opts::BINARY
+    };
     assert_eq!(999.file_size(semi_custom_options2).unwrap(), "999 B/s");
 
     let semi_custom_options3 = file_size_opts::FileSizeOpts {
@@ -269,12 +300,31 @@ fn test_sizes() {
         fixed_at: file_size_opts::FixedAt::Kilo,
         ..file_size_opts::BINARY
     };
-    assert_eq!(16584975.file_size(semi_custom_options5).unwrap(), "16196.26 KiB");
+    assert_eq!(
+        16584975.file_size(semi_custom_options5).unwrap(),
+        "16196.26 KiB"
+    );
 
     let semi_custom_options6 = file_size_opts::FileSizeOpts {
         fixed_at: file_size_opts::FixedAt::Tera,
         decimal_places: 10,
         ..file_size_opts::BINARY
     };
-    assert_eq!(15284975.file_size(semi_custom_options6).unwrap(), "0.0000139016 TiB");
+    assert_eq!(
+        15284975.file_size(semi_custom_options6).unwrap(),
+        "0.0000139016 TiB"
+    );
+
+    let semi_custom_options7 = file_size_opts::FileSizeOpts {
+        allow_negative: true,
+        ..file_size_opts::DECIMAL
+    };
+    assert_eq!(
+        (-5500).file_size(&semi_custom_options7).unwrap(),
+        "-5.50 KB"
+    );
+    assert_eq!(
+        (5500).file_size(&semi_custom_options7).unwrap(),
+        "5.50 KB"
+    );
 }
